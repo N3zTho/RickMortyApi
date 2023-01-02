81645fe256e8cf51c0935fdf8896382c247f3162
Ernesto <reddonet@gmail.com>
2023-01-02 11:29:57 -0500

    Initial Commit

---
+++ RickMortyApi/RickMortyApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyApiApp.swift
+//  RickMortyApi
+//
+//  Created by Ernesto Redonet on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
